ab94cfa097af55bde504c70adc370252ceddbb58
rghanimah <rabiaghanimah@gmail.com>
2018-08-07 14:38:10 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,9 +2,9 @@ puts "What is your name?"
 guest_name = gets.chomp
 # Try starting out with puts'ing a string.
 puts "#{guest_name}"
-puts "what is the date of your party?"
-date = gets.chomp
-puts "what is the time your party is at?"
-time = gets.chomp
-puts "what is the name of your party?"
-party_name = gets.chomp
+# puts "what is the date of your party?"
+# date = gets.chomp
+# puts "what is the time your party is at?"
+# time = gets.chomp
+# puts "what is the name of your party?"
+# party_name = gets.chomp
